8247c7b2e54f8d9f93feb4d47da738819b72774d
Sergey Belov <sabelov@rambler.ru>
2019-12-14 12:34:36 +0300

    Initial commit

---
+++ src/main/java/TestGit/App.java
@@ -0,0 +1,19 @@
+package TestGit;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+
+        System.out.println( "Hello World!" );
+
+        System.out.println("Добавлена строка 1");
+        System.out.println("Добавлена строка 2");
+        System.out.println("Добавлена строка 3");
+        System.out.println("Добавлена строка 4");
+    }
+}
